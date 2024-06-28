22d83df88ae24d11dea1147b665c3759b58d0000
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-06-28 19:41:32 +0500

    TRIPMA with flights, pinfo

---
+++ app.js
@@ -0,0 +1,68 @@
+// 1. uchta sondan eng kattasini chiqaruvchi algaritm
+
+
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let c = +prompt("Uchinchi sonni kiriting");
+
+// if(a > b & a > c){
+//     alert(`Eng katta son bu birinchi kiritgan soningiz ya'ni: ${a}`)
+// } else if(b > a & b > c){
+//     alert(`Eng katta son bu ikkinchi kiritgan soningiz ya'ni: ${b}`)
+// } else if(c > a & c > b){
+//     alert(`Eng katta son bu uchinchi kiritgan soningiz ya'ni: ${c}`)
+// } else {
+//     alert("Error")
+// }
+
+
+
+//  2. foydalanuvchi mablag’ini kiritadi va mehmonhonada olinadigan hona turini ayting 1000 - lux | 500 - yarim lux yoki 500dan 200 gacha bo’lsa odiy hona 200 dan kam bo’lsa - joy yo’q 
+
+
+// let balance = +prompt("Mabalag'ni kiriting");
+
+// if (balance == 1000){
+//     alert("Lux")
+// } else if(balance == 500){
+//     alert("Semi-Lux")
+// } else if(balance >= 200 & balance <= 500){
+//     alert("Oddiy xona")
+// } else if(balance < 200){
+//     alert("Xona mavjud emas")
+// } else{
+//     alert("Ushbu summaga mos xona mavjud emas ")
+// }
+
+
+
+//  3. foydalanucvchi son kiritadi agar son 3 ga bo’linsa fizz 5 ga bo’lisa bass agar 3 ham 5 ga bo’linsa fizz-bazz 
+
+// let a = +prompt("Son kiriting");
+
+// if(a % 3 == 0 & a % 5 == 0 ){
+//     alert("fizz-bass")
+// } else if (a % 3 == 0){
+//     alert("fizz")
+// } else if (a % 5 == 0){
+//     alert("bazz")
+// }
+
+
+
+
+//  4. uchta sondan eng kichigini chiqaruvchi algaritm
+
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let c = +prompt("Uchinchi sonni kiriting");
+
+// if (a < b & a < c) {
+//     alert(`Eng kichik son bu birinchi kiritgan soningiz ya'ni: ${a}`)
+// } else if (b < a & b < c) {
+//     alert(`Eng kichik son bu ikkinchi kiritgan soningiz ya'ni: ${b}`)
+// } else if (c < a & c < b) {
+//     alert(`Eng kichik son bu uchinchi kiritgan soningiz ya'ni: ${c}`)
+// } else {
+//     alert("Error")
+// }
